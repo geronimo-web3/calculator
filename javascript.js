b30aa163c3828b9a3ff390c68009826510373225
geronimo-web3 <web3@finsol.ch>
2022-07-05 11:32:39 +0200

    finished

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let calcStatus = 0;
+let calcResult = 0;
 let firstNumber = "";
 let secondNumber = "";
 let operator = ""; 
@@ -32,29 +33,68 @@ function calculation(){
             firstNumber += userInput;
         // if the decimal sign is clicked then show 0. in case no number proceeds otherwise attache id
         } else if (userInput == "decimal"){
-            if (firstNumber == ""){
-                firstNumber = "0.";
-            } else {
-                firstNumber += ".";
-            }
-            // disable decimal event handler if selected once
-            switchDecimalButtonHandler(firstNumber);
-            //if the user clicked delete the slice last character
+            firstNumber = addDecimalSignToNumber(firstNumber);
+        //if the user clicked delete the slice last character
         } else if (userInput == "delete"){
-            if (firstNumber != ""){
-                let str = firstNumber;
-                firstNumber = str.slice(0, str.length - 1);
-                switchDecimalButtonHandler(firstNumber);
-                console.log(firstNumber); 
-            }
+            firstNumber = deleteCharFromString(firstNumber);
+        } else if (userInput == "+" || userInput == "-" || userInput == "*" || userInput == "/"){
+            operator = userInput;
+            calcStatus = 1;
         }
+    } else if (calcStatus === 1){
+        if ((userInput == "+" || userInput == "-" || userInput == "*" || userInput == "/") && secondNumber === ""){
+            operator = userInput;
+        } else if (isNaN(userInput) == false){
+            secondNumber += userInput;
+        } else if (userInput == "decimal"){
+            secondNumber = addDecimalSignToNumber(secondNumber);
+        //if the user clicked delete the slice last character
+        } else if (userInput == "delete"){
+            secondNumber = deleteCharFromString(secondNumber);
+        } else if ((userInput == "+" || userInput == "-" || userInput == "*" || userInput == "/") && secondNumber != ""){
+            calcResult = calculateResult(operator);
+            firstNumber = calcResult;
+            secondNumber = "";
+            operator = userInput;
+        } else if (userInput == "equal" && secondNumber != ""){
+            calcResult = calculateResult(operator);
+            firstNumber = calcResult;
+            secondNumber = "";
+            operator = "";
+        } 
+    }
 
-
-        calcPanel.innerHTML = firstNumber;
-    } 
+    calcPanel.innerHTML = `${firstNumber} ${operator} ${secondNumber}` ;
+    resultPanel.innerHTML = calcResult ;
   
 }
 
+function calculateResult(operat){
+    let opResult = 0;
+
+    switch(operat){
+        case "+":
+            opResult = parseFloat(firstNumber) + parseFloat(secondNumber)
+            return opResult;
+        case "-":
+            opResult = parseFloat(firstNumber) - parseFloat(secondNumber)
+            secondNumber = "";
+            operator = "";
+            return opResult;
+        case "*":
+            opResult = parseFloat(firstNumber) * parseFloat(secondNumber)
+            secondNumber = "";
+            operator = "";
+            return opResult;
+        case "/":
+            opResult = parseFloat(firstNumber) / parseFloat(secondNumber)
+            secondNumber = "";
+            operator = "";
+            return opResult;
+    }
+}
+
+// Clear all for new calculation
 function clearCalculation(){
     const calcPanel = document.querySelector("#panelRow01");
     const resultPanel = document.querySelector("#panelRow02");
@@ -64,6 +104,7 @@ function clearCalculation(){
  
     addEventToBtn();
     calcStatus = 0;
+    calcResult = 0;
     firstNumber = "";
     secondNumber = "";
     operator = ""; 
@@ -78,5 +119,28 @@ function switchDecimalButtonHandler(number){
     } else {
         btn.addEventListener("click", calculation);
     }
+}
 
+// Delete the last character from string. If string is 0. delete all
+function deleteCharFromString(number){
+    if (number == "0."){
+        number = "";
+        switchDecimalButtonHandler(number);
+    } else if (number != ""){
+        let str = number;
+        number = str.slice(0, str.length - 1);
+        switchDecimalButtonHandler(number); 
+    }
+    return number;
+}
+
+function addDecimalSignToNumber(number){
+    if (number == ""){
+        number = "0.";
+    } else {
+        number += ".";
+    }
+    // disable decimal event handler if selected once
+    switchDecimalButtonHandler(number);
+    return number;
 }
